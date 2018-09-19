115af759fd6a2301957fed8f9957a59f49ee246a
Duy Dam <dpducduy10l@gmail.com>
2018-09-19 09:21:07 +0700

    Send DialogFlow response message to server (8080).

--- ctsa-chatbot/helpers/processMessage.js
+++ ctsa-chatbot/helpers/processMessage.js
@@ -1,17 +1,17 @@
 const API_AI_TOKEN = '24d58b6fb00e4cb4b05db32d3dd462ce';
 const apiAiClient = require('apiai')(API_AI_TOKEN);
-const FACEBOOK_ACCESS_TOKEN = 'EAAHywk70sd4BAL6jZCWkLZC26fRDfq8g494on2xpXYnM5rs0ZC5TDgZCj61VBfEC2IZASI5NO1dD9zfS6HhucnVc8n7OuMhAh9aXWb7v8LbjvptdKSWcH8Lez0FUh5ZAoUW8ebBwJ9rCosfe5xKDrYuJtKrzgQQY8pcqw9PGIuLwZDZD';
+const FACEBOOK_ACCESS_TOKEN = 'EAAHywk70sd4BAGSgyDZA0WYKFv6fZB9aF9obIjpIsT8watzff4E58dhV36ZAxGL0GgDTpY8goUd3XmDcXbz4QgzSRHYWSzriwb7Y7lBFr4apZAGBtBqXQlNxbiBWqpCZBdP8HhUVXXCR2uPUS3YDVzwZBHg0ch4tHuNfmAlZCbreQZDZD';
 const request = require('request');
 const sendTextMessage = (senderId, text) => {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+        qs: { access_token: FACEBOOK_ACCESS_TOKEN },
         method: 'POST',
         json: {
             recipient: { id: senderId },
             message: { text },
         }
- });
+    });
 };
 
 module.exports = (event) => {
@@ -21,6 +21,17 @@ module.exports = (event) => {
     apiaiSession.on('response', (response) => {
         const result = response.result.fulfillment.speech;
         sendTextMessage(senderId, result);
+        // console.log(response);
+        request.post({
+            url: 'http://localhost:8080/message',
+            json: { msg : message }
+        },
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body);
+                }
+            }
+        );
     });
     apiaiSession.on('error', error => console.log(error));
     apiaiSession.end();
