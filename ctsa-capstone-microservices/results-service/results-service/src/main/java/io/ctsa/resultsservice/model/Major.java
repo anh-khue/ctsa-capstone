a71f284b3c7a02b869bfa2ca791c057f6dfe7ea4
Duy Dam <dpducduy10l@gmail.com>
2018-10-07 12:01:51 +0700

    Code skeleton for results service.

---
+++ ctsa-capstone-microservices/results-service/results-service/src/main/java/io/ctsa/resultsservice/model/Major.java
@@ -0,0 +1,39 @@
+package io.ctsa.resultsservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Major {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NonNull
+    private String name;
+    @NonNull
+    private int businessFieldId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Major major = (Major) o;
+        return id == major.id &&
+                businessFieldId == major.businessFieldId &&
+                Objects.equals(name, major.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name, businessFieldId);
+    }
+}
