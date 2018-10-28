e4ac3fe1b86299576c70414e7526befcd61849eb
anh-khue <thailyanhkhue@gmail.com>
2018-10-28 21:56:31 +0700

    Modify models in Elasticsearch Service for matching with Careers Service.

--- ctsa-capstone-microservices/elasticsearch-service/src/main/java/io/ctsa/elasticsearchservice/model/Keyword.java
+++ ctsa-capstone-microservices/elasticsearch-service/src/main/java/io/ctsa/elasticsearchservice/model/Keyword.java
@@ -1,6 +1,12 @@
 package io.ctsa.elasticsearchservice.model;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
 
 @Data
 @NoArgsConstructor
@@ -9,11 +15,15 @@ import lombok.*;
 public class Keyword {
 
     private int id;
-    @NonNull
     private String word;
     private boolean isSynonym;
     private Integer mainKeywordId;
     private Integer positionId;
     private Integer skillId;
     private boolean pushedToElasticsearch;
+    @JsonIgnore
+    private Keyword mainKeyword;
+    private Collection<Keyword> synonyms;
+//    private Position positionByPositionId;
+//    private Skill skillBySkillId;
 }
