4346c441bf03ab2213e4291628d15be7b8073f50
anh-khue <thailyanhkhue@gmail.com>
2018-11-10 16:16:42 +0700

    Add constants for accessing services through API Gateway.

--- web-ui/web-v0.8/ctsa-webapp/src/main/resources/static/js/entrance-exam.js
+++ web-ui/web-v0.8/ctsa-webapp/src/main/resources/static/js/entrance-exam.js
@@ -1,102 +1,102 @@
 function retrieveSubjects(modalId) {
-  let count = 0;
+    let count = 0;
 
-  let modal = $("#" + modalId);
+    let modal = $("#" + modalId);
 
-  let entranceExamSubjects = [];
+    let entranceExamSubjects = [];
 
-  axios
-    .get("http://localhost:8005/entrance-exam-subjects")
-    .then(response => {
-      let subjects = response.data;
-      let subjectsPerCol = Math.ceil(subjects.length / 2);
-      let col = $("<div>", {
-        class: "col-md-6 pl-4"
-      });
+    axios
+        .get(API_GATEWAY + RESULTS_SUGGESTIONS_SERVICE + "/entrance-exam-subjects")
+        .then(response => {
+            let subjects = response.data;
+            let subjectsPerCol = Math.ceil(subjects.length / 2);
+            let col = $("<div>", {
+                class: "col-md-6 pl-4"
+            });
 
-      subjects.forEach(subject => {
-        entranceExamSubjects.push(subject);
+            subjects.forEach(subject => {
+                entranceExamSubjects.push(subject);
 
-        count++;
+                count++;
 
-        createSubjectInput(subject).appendTo(col);
+                createSubjectInput(subject).appendTo(col);
 
-        if (count % subjectsPerCol === 0) {
-          col.appendTo(modal);
-          col = $("<div>", {
-            class: "col-md-6 pl-4"
-          });
-        }
-      });
-    })
-    .then(() => {
-      sessionStorage.setItem("entranceExamSubjects", JSON.stringify(entranceExamSubjects));
-      // Initialize Sliders
-      var sliderSections = document.getElementsByClassName("range-slider");
-      for (var x = 0; x < sliderSections.length; x++) {
-        var sliders = sliderSections[x].getElementsByTagName("input");
-        for (var y = 0; y < sliders.length; y++) {
-          if (sliders[y].type === "range") {
-            sliders[y].oninput = getVals;
-            // Manually trigger event first time to display values
-            sliders[y].oninput();
-          }
-        }
-      }
-    });
+                if (count % subjectsPerCol === 0) {
+                    col.appendTo(modal);
+                    col = $("<div>", {
+                        class: "col-md-6 pl-4"
+                    });
+                }
+            });
+        })
+        .then(() => {
+            sessionStorage.setItem("entranceExamSubjects", JSON.stringify(entranceExamSubjects));
+            // Initialize Sliders
+            var sliderSections = document.getElementsByClassName("range-slider");
+            for (var x = 0; x < sliderSections.length; x++) {
+                var sliders = sliderSections[x].getElementsByTagName("input");
+                for (var y = 0; y < sliders.length; y++) {
+                    if (sliders[y].type === "range") {
+                        sliders[y].oninput = getVals;
+                        // Manually trigger event first time to display values
+                        sliders[y].oninput();
+                    }
+                }
+            }
+        });
 }
 
 function createSubjectInput(subject) {
-  let subjectDiv = $("<div>", {
-    id: subject.name + "Mark",
-    class: "collegeScoreInput"
-  });
+    let subjectDiv = $("<div>", {
+        id: subject.name + "Mark",
+        class: "collegeScoreInput"
+    });
 
-  $("<span>", {
-    text: subject.vietnamese + " "
-  }).appendTo(subjectDiv);
+    $("<span>", {
+        text: subject.vietnamese + " "
+    }).appendTo(subjectDiv);
 
-  $("<input>", {
-    class: "rangeValues"
-  }).appendTo(subjectDiv);
+    $("<input>", {
+        class: "rangeValues"
+    }).appendTo(subjectDiv);
 
-  if (!subject.required) {
-    let label = $("<label>", {
-      class: "float-right"
+    if (!subject.required) {
+        let label = $("<label>", {
+            class: "float-right"
+        });
+
+        $("<input>", {
+            type: "checkbox",
+            value: "",
+            onchange: "disableScoreInput(this)"
+        }).appendTo(label);
+        label.append("  Không thi");
+
+        label.appendTo(subjectDiv);
+    }
+
+    let section = $("<section>", {
+        style: "padding-bottom:20px",
+        class: "range-slider"
     });
 
     $("<input>", {
-      type: "checkbox",
-      value: "",
-      onchange: "disableScoreInput(this)"
-    }).appendTo(label);
-    label.append("  Không thi");
+        value: "5",
+        min: "0",
+        max: "10",
+        step: "0.1",
+        type: "range"
+    }).appendTo(section);
 
-    label.appendTo(subjectDiv);
-  }
+    $("<input>", {
+        value: "10",
+        min: "0",
+        max: "10",
+        step: "0.1",
+        type: "range"
+    }).appendTo(section);
 
-  let section = $("<section>", {
-    style: "padding-bottom:20px",
-    class: "range-slider"
-  });
+    section.appendTo(subjectDiv);
 
-  $("<input>", {
-    value: "5",
-    min: "0",
-    max: "10",
-    step: "0.1",
-    type: "range"
-  }).appendTo(section);
-
-  $("<input>", {
-    value: "10",
-    min: "0",
-    max: "10",
-    step: "0.1",
-    type: "range"
-  }).appendTo(section);
-
-  section.appendTo(subjectDiv);
-
-  return subjectDiv;
+    return subjectDiv;
 }
---
+++ web-ui/web-v0.8/ctsa-webapp/src/main/resources/static/js/services-routes.js
@@ -0,0 +1,7 @@
+const API_GATEWAY = 'http://localhost:8080/';
+const CAREERS_SERVICES = 'careers-service';
+const CAREER_TREND_SERVICE = 'career-trend-service';
+const COMPANY_MANAGEMENT = 'company-management';
+const ELASTICSEARCH_SERVICE = 'elasticsearch-service';
+const RESULTS_SUGGESTIONS_SERVICE = 'results-suggestions-service';
+const WAREHOUSE_SERVICE = 'warehouse-service';
